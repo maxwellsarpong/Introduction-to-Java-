2c9bc61807c9dc5a96c9527f63227efedb6dc543
maxwellsarpong <maxwellsarpong07@gmail.com>
2021-09-10 12:19:54 +0000

    threads

---
+++ ThreadsJava.java
@@ -0,0 +1,22 @@
+public class ThreadsJava {
+    
+    public static void main(String[] args){
+        /*
+
+            Java Threads
+            Threads allows a program to operate more efficiently by doing multiple things at the same time.
+
+            Threads can be used to perform complicated tasks in the background without 
+            interrupting the main program.
+
+            Creating a Thread
+            There are two ways to create a thread.
+
+            It can be created by extending the Thread class and overriding its run() method:
+        */
+
+        
+
+
+    }
+}
